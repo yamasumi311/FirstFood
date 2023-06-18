a9775defbb9503c152eac55782351bb003045c36
yamasumi311 <chim.0.don@gmail.com>
2023-06-18 10:18:05 +1000

    saving to file but doesn't work

--- FirstFood.py
+++ FirstFood.py
@@ -1,14 +1,28 @@
 # baby's name
 baby_name = input('Enter baby name: ')
-
-# open FoodList file in append mode
 file_name = 'FoodList'
-index = 0
-with open(file_name, 'a') as file:
-    # write a new food to the file
-    new_food = input('Enter new food: ')
-    index =+ 1
-    file.write(f"{index}.{new_food} + \n")
 
+def add_items():
+    # open FoodList file in append mode
+    index = 0
+    with open(file_name, 'a') as file:
+        # write a new food to the file
+        new_food = input('Enter new food: ')
+        if check_items(new_food):
+            index =+ 1
+            file.write(f"{index}.{new_food}\n")
+            return index, new_food
+        return print('This item is already registered!')
+
+# check items so that there is no duplication
+def check_items(user_input):
+    with open(file_name, 'r') as file:
+        for i in file:
+            item = i.pop()
+            if item == user_input:
+                return False
+        return True
+
+add_items()
 # show how many foods have been tried
 print(f'{baby_name} has had {index} foods!!')
