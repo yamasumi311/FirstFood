32981eb2a882a4608ee5db9d9e37a23de2dac69d
yamasumi311 <chim.0.don@gmail.com>
2023-06-21 22:21:14 +1000

    hide and appear the form on submit

--- HTML/script.js
+++ HTML/script.js
@@ -1,5 +1,5 @@
 function read(baby_name) {
-    fetch('/baby/' + baby_name)
+    return fetch('/baby/' + baby_name)
         .then(function (response) {
             return response.json()
         })
@@ -15,11 +15,26 @@ function read(baby_name) {
         })
 }
 
+function read_baby_name_file(event) {
+    event.preventDefault();
+    const baby_name = event.target.elements.baby_name.value
+    read(baby_name).then(function () {
+        register_adding_new_food(baby_name)
+        const header = document.getElementById('baby_name')
+        header.classList.remove('hidden')
+        header.querySelector('h1').textContent = baby_name
+        document.getElementById('form_baby_name').classList.add('hidden')
+    })
+}
 
-document.getElementById('form')
-    .addEventListener('submit', function (event) {
+document.getElementById('form_baby_name')
+    .addEventListener('submit', read_baby_name_file)
+
+function register_adding_new_food(baby_name) {
+    document.getElementById('form_new_food').classList.remove('hidden')
+
+    function add_new_food(event) {
         event.preventDefault();
-        const baby_name = event.target.elements.baby_name.value
         const new_food = event.target.elements.new_food.value
         fetch('/baby/' + baby_name, { method: 'POST', body: new_food })
             .then(function (response) {
@@ -29,4 +44,8 @@ document.getElementById('form')
                 console.log(p)
                 read(baby_name)
             })
-    })
+    }
+
+    document.getElementById('form_new_food')
+        .addEventListener('submit', add_new_food)
+}
