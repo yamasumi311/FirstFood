a76aba9970ca21619a76ea7d657b3dbe434932b6
yamasumi311 <chim.0.don@gmail.com>
2023-06-27 19:38:33 +1000

    get json object to html

--- HTML/script.js
+++ HTML/script.js
@@ -82,3 +82,65 @@ for (let i = 0; i < coll.length; i++) {
 
     })
 }
+
+const categories = {
+    "Dairy": [
+        "yogurt"
+    ],
+    "Fruits": [
+        "apple",
+        "banana",
+        "Mandarine"
+    ],
+    "Grains/Roots": [
+        "rice"
+    ],
+    "Legumes": [],
+    "Meat/Fish/Eggs": [],
+    "Natural Seasonings": [],
+    "Seeds/Nuts": [],
+    "Vegetables": []
+}
+
+function turn_json_object_to_html(categories) {
+    const category_names = Object.keys(categories)
+    const root = document.getElementById("list")
+    for (let i = 0; i < category_names.length; i++) {
+        const category = category_names[i]
+        const foods = categories[category]
+        const button_element = create_button_element(category)
+        const div_element = create_div_element()
+        const ul = div_element.querySelector("ul")
+        root.appendChild(button_element)
+        root.appendChild(div_element)
+        for (let j = 0; j < foods.length; j++) {
+            const food = foods[j]
+            const f = create_food_element(food)
+            ul.appendChild(f)
+        }
+    }
+}
+
+function create_food_element(food_name) {
+    const food = document.createElement("li")
+    food.textContent = food_name
+    return food
+}
+
+function create_button_element(category_name) {
+    const button = document.createElement("button")
+    button.setAttribute("type", "button")
+    button.classList.add("collapsible")
+    button.textContent = category_name
+    return button
+}
+
+function create_div_element() {
+    const div = document.createElement("div")
+    div.classList.add("foods")
+    const ul = document.createElement("ul")
+    div.appendChild(ul)
+    return div
+}
+
+turn_json_object_to_html(categories)
